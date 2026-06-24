aeafb8c72ab600952068e70851c5ce76f5587890
javiermunrom <javiermunoz360@gmail.com>
2026-06-24 17:12:08 +0200

    chore: reset project to Astro base

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,5 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import { fileURLToPath } from "node:url";
-
-import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-    resolve: {
-      alias: {
-        "@": fileURLToPath(new URL("./src", import.meta.url)),
-        "@components": fileURLToPath(
-          new URL("./src/components", import.meta.url),
-        ),
-        "@content": fileURLToPath(new URL("./src/content", import.meta.url)),
-        "@data": fileURLToPath(new URL("./src/data", import.meta.url)),
-        "@layouts": fileURLToPath(new URL("./src/layouts", import.meta.url)),
-        "@styles": fileURLToPath(new URL("./src/styles", import.meta.url)),
-        "@types": fileURLToPath(new URL("./src/types", import.meta.url)),
-        "@utils": fileURLToPath(new URL("./src/utils", import.meta.url)),
-      },
-    },
-  },
-});
+export default defineConfig({});
--- src/utils/projects.ts
+++
@@ -1,9 +0,0 @@
-import { getCollection, type CollectionEntry } from "astro:content";
-
-export type ProjectEntry = CollectionEntry<"projects">;
-
-export async function getProjects(): Promise<ProjectEntry[]> {
-  const projects = await getCollection("projects");
-
-  return projects.sort((a, b) => b.data.date.getTime() - a.data.date.getTime());
-}
